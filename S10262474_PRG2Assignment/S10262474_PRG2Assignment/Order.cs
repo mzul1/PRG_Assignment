e80f58aec8fe29a99f8cbe0eb59ec302a50e0075
JinXuan03 <148025005+JinXuan03@users.noreply.github.com>
2024-01-22 14:56:03 +0800

    Update Order.cs

--- S10262474_PRG2Assignment/S10262474_PRG2Assignment/Order.cs
+++ S10262474_PRG2Assignment/S10262474_PRG2Assignment/Order.cs
@@ -6,62 +6,82 @@ using System.Threading.Tasks;
 
 namespace S10262474_PRG2Assignment
 {
-    public enum IceCreamOption
-    {
-        Cup,
-        Cone,
-        Waffle
-    }
-    public class Order
+    class Order
     {
         public int Id { get; set; }
         public DateTime TimeReceived { get; set; }
         public DateTime? TimeFulfilled { get; set; }
-        public List<IceCream> IceCreamList { get; set; }
-
-        public Order()
-        {
-            IceCreamList = new List<IceCream>();
-        }
-
-        public Order(int id, DateTime timeReceived) : this()
+        public List<IceCream> IceCreamList { get; set; } = new List<IceCream>();
+        public Order() { }
+        public Order(int id, DateTime timereceived)
         {
             Id = id;
-            TimeReceived = timeReceived;
+            TimeReceived = timereceived;
         }
+        public void ModifyIceCream(int orderid)
+        {
+            int orderno = orderid - 1;
 
+            // Update basic order details
+            Console.Write("Enter new option: ");
+            IceCreamList[orderno].Option = Console.ReadLine();
+
+            Console.Write("Enter new number of scoops: ");
+            IceCreamList[orderno].Scoops = Convert.ToInt32(Console.ReadLine());
+
+            // Clear existing flavours and add new ones
+            IceCreamList[orderno].Flavours.Clear();
+            Console.Write("Enter new flavour (or NIL to stop adding): ");
+            string flavourtype = Console.ReadLine();
+            while (flavourtype != "nil")
+            {
+                Console.Write("Is it premium? (True/False): ");
+                bool flavourpremium = Convert.ToBoolean(Console.ReadLine());
+                Console.Write("Enter new flavour quantity: ");
+                int flavourquantity = Convert.ToInt32(Console.ReadLine());
+
+                Flavour flavour = new Flavour(flavourtype, flavourpremium, flavourquantity);
+                IceCreamList[orderno].Flavours.Add(flavour);
+
+                Console.Write("Enter new flavour (or NIL to stop adding): ");
+                flavourtype = Console.ReadLine();
+            }
+
+            // Clear existing toppings and add new ones
+            IceCreamList[orderno].Toppings.Clear();
+            Console.Write("Enter new topping (or NIL to stop adding): ");
+            string toppingtype = Console.ReadLine();
+            while (toppingtype != "nil")
+            {
+                Topping topping = new Topping(toppingtype);
+                IceCreamList[orderno].Toppings.Add(topping);
+
+                Console.Write("Enter new topping (or NIL to stop adding): ");
+                toppingtype = Console.ReadLine();
+            }
+        }
         public void AddIceCream(IceCream iceCream)
         {
             IceCreamList.Add(iceCream);
         }
-
-        public void ModifyIceCream(int index)
+        public void DeleteIceCream(int num)
         {
-            
-            throw new NotImplementedException();
+            IceCreamList.RemoveAt(num);
         }
-
-        public void DeleteIceCream(int index)
-        {
-            if (index >= 0 && index < IceCreamList.Count)
-            {
-                IceCreamList.RemoveAt(index);
-            }
-            else
-            {
-                throw new IndexOutOfRangeException("Invalid ice cream index.");
-            }
-        }
-
         public double CalculateTotal()
         {
-            return IceCreamList.Sum(iceCream => iceCream.CalculatePrice());
+            double total = 0;
+            foreach (IceCream icecream in IceCreamList)
+            {
+                total += icecream.CalculatePrice();
+            }
+            return total;
         }
-
         public override string ToString()
         {
-            string iceCreamsText = string.Join("\n", IceCreamList.Select(iceCream => iceCream.ToString()));
-            return $"Order ID: {Id}, Time Received: {TimeReceived}, Ice Creams:\n{iceCreamsText}, Total Price: {CalculateTotal():C2}";
+            //return "ID: " + Id + "\tTime Received: " + TimeReceived + "\tTime Fulfilled: " + TimeFulfilled + "\tIceCreamList: " + IceCreamList.ToString();
+            string iceCreamDetails = string.Join("; ", IceCreamList.Select(iceCream => iceCream.ToString()));
+            return $"ID: {Id}   Time Received: {TimeReceived:dd/MM/yyyy h:mm:ss tt}    Time Fulfilled: {TimeFulfilled?.ToString("dd/MM/yyyy h:mm:ss tt")} IceCreamList: {iceCreamDetails}";
         }
     }
 }
