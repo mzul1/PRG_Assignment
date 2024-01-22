d82c7221166355c4a57c941c071da6c3f415d032
JinXuan03 <148025005+JinXuan03@users.noreply.github.com>
2024-01-22 14:56:50 +0800

    Update Topping.cs

--- S10262474_PRG2Assignment/S10262474_PRG2Assignment/Topping.cs
+++ S10262474_PRG2Assignment/S10262474_PRG2Assignment/Topping.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,27 +6,17 @@ using System.Threading.Tasks;
 
 namespace S10262474_PRG2Assignment
 {
-    public class Topping
+    internal class Topping
     {
         public string Type { get; set; }
-        public double Cost { get; } = 1.00;
-
         public Topping() { }
-
         public Topping(string type)
         {
             Type = type;
         }
-
-        /*public double CalculatePrice()
-    {
-        // Each topping costs $1.
-        return 1;
-    }*/
-
         public override string ToString()
         {
-            return Type;
+            return "Type: " + Type;
         }
     }
 }
