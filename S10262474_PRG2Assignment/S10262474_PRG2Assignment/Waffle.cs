a95eb2e5d44b6a4661849fc4a1d971f4ca49a7f8
JinXuan03 <148025005+JinXuan03@users.noreply.github.com>
2024-01-22 14:57:15 +0800

    Update Waffle.cs

--- S10262474_PRG2Assignment/S10262474_PRG2Assignment/Waffle.cs
+++ S10262474_PRG2Assignment/S10262474_PRG2Assignment/Waffle.cs
@@ -6,45 +6,54 @@ using System.Threading.Tasks;
 
 namespace S10262474_PRG2Assignment
 {
-    public class Waffle : IceCream
+    class Waffle : IceCream
     {
         public string WaffleFlavour { get; set; }
-
-        /*public Waffle(string option, int scoops, List<Flavour> flavours, List<Topping> toppings, string waffleFlavour)
-            : base(option, scoops, flavours, toppings)
+        public Waffle() : base() { }
+        public Waffle(string option, int scoops, List<Flavour> flavours,
+            List<Topping> toppings, string waffleflavour) : base(option, scoops, flavours, toppings)
         {
-            WaffleFlavour = waffleFlavour;
+            WaffleFlavour = waffleflavour;
         }
-
         public override double CalculatePrice()
         {
-            double price = base.CalculatePrice() + 3; 
-            if (new[] { "Red velvet", "Charcoal", "Pandan" }.Contains(WaffleFlavour))
+            double price;
+            if (base.Scoops == 1)
             {
-                price += 3; 
+                price = 7.00;
+            }
+            else if (base.Scoops == 2)
+            {
+                price = 8.50;
+            }
+            else
+            {
+                price = 9.50;
+            }
+
+            foreach (Flavour flavour in base.Flavours)
+            {
+                if (flavour.Premium)
+                {
+                    price += 2 * flavour.Quantity;
+                }
+            }
+
+            if (base.Toppings.Count > 0)
+            {
+                price += base.Toppings.Count * 1;
+            }
+
+            if (WaffleFlavour == "Red Velvet" || WaffleFlavour == "Charcoal" ||
+                WaffleFlavour == "Pandan")
+            {
+                price += 3;
             }
             return price;
-        }*/
-
-        public Waffle(int scoops, List<Flavour> flavours, List<Topping> toppings, string waffleFlavour)
-        : base("Waffle", scoops, flavours, toppings)
-        {
-            WaffleFlavour = waffleFlavour;
         }
-
-        public override double CalculatePrice()
-        {
-            double price = base.CalculatePrice() + 3; // Base cost for waffle option.
-            if (WaffleFlavour == "Red Velvet" || WaffleFlavour == "Charcoal" || WaffleFlavour == "Pandan")
-            {
-                price += 3; // Additional cost for special waffle flavours.
-            }
-            return price;
-        }
-
         public override string ToString()
         {
-            return base.ToString() + $", Waffle Flavour: {WaffleFlavour}";
+            return base.ToString() + "\tWaffle Flavour: " + WaffleFlavour;
         }
     }
-}
+  }
