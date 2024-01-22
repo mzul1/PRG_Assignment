88b50d31b10e02fa2cdedfd7ff920a8d91f7aca9
JinXuan03 <148025005+JinXuan03@users.noreply.github.com>
2024-01-22 14:53:02 +0800

    Update Cone.cs

--- S10262474_PRG2Assignment/S10262474_PRG2Assignment/Cone.cs
+++ S10262474_PRG2Assignment/S10262474_PRG2Assignment/Cone.cs
@@ -6,35 +6,53 @@ using System.Threading.Tasks;
 
 namespace S10262474_PRG2Assignment
 {
-    public class Cone : IceCream
+    class Cone : IceCream
     {
         public bool Dipped { get; set; }
-
-        /*public Cone(string option, int scoops, List<Flavour> flavours, List<Topping> toppings, bool dipped)
-            : base(option, scoops, flavours, toppings)
-        {
-            Dipped = dipped;
-        }*/
-
-        public Cone(int scoops, List<Flavour> flavours, List<Topping> toppings, bool dipped)
-        : base("Cone", scoops, flavours, toppings)
+        public Cone() : base() { }
+        public Cone(string option, int scoops, List<Flavour> flavours,
+            List<Topping> toppings, bool dipped) : base(option, scoops, flavours, toppings)
         {
             Dipped = dipped;
         }
-
         public override double CalculatePrice()
         {
-            double price = base.CalculatePrice();
+            double price;
+            if (base.Scoops == 1)
+            {
+                price = 4.00;
+            }
+            else if (base.Scoops == 2)
+            {
+                price = 5.50;
+            }
+            else
+            {
+                price = 6.50;
+            }
+
+            foreach (Flavour flavour in base.Flavours)
+            {
+                if (flavour.Premium)
+                {
+                    price += 2 * flavour.Quantity;
+                }
+            }
+
+            if (base.Toppings.Count > 0)
+            {
+                price += base.Toppings.Count * 1;
+            }
+
             if (Dipped)
             {
-                price += 2; 
+                price += 2;
             }
             return price;
         }
-
         public override string ToString()
         {
-            return base.ToString() + (Dipped ? ", Chocolate-Dipped" : "");
+            return base.ToString() + "\tDipped: " + Dipped;
         }
     }
 }
