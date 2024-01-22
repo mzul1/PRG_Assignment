6f31c6b22fabe6737291f405107440267a9e32bc
JinXuan03 <148025005+JinXuan03@users.noreply.github.com>
2024-01-22 14:54:28 +0800

    Update Flavour.cs

--- S10262474_PRG2Assignment/S10262474_PRG2Assignment/Flavour.cs
+++ S10262474_PRG2Assignment/S10262474_PRG2Assignment/Flavour.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,31 +6,21 @@ using System.Threading.Tasks;
 
 namespace S10262474_PRG2Assignment
 {
-    public class Flavour
+    internal class Flavour
     {
         public string Type { get; set; }
         public bool Premium { get; set; }
         public int Quantity { get; set; }
-        public double Cost => Premium ? 2.00 : 0.00;
-
         public Flavour() { }
-
         public Flavour(string type, bool premium, int quantity)
         {
             Type = type;
             Premium = premium;
             Quantity = quantity;
         }
-
-        /*public double CalculatePrice()
-        {
-            // Premium flavours cost an additional $2 per scoop.
-            return Quantity * (Premium ? 2 : 0);
-        }*/
-
         public override string ToString()
         {
-            return $"{Type}{(Premium ? " (Premium)" : "")} x {Quantity}";
+            return "Type:" + Type + "\tPremium: " + Premium + "\tQuantity: " + Quantity;
         }
     }
 }
