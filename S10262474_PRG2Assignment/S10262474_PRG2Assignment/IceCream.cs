ed4d8ef3314dbb99edabb835031ea4687a5c1785
JinXuan03 <148025005+JinXuan03@users.noreply.github.com>
2024-01-30 13:07:03 +0800

    Update IceCream.cs

--- S10262474_PRG2Assignment/S10262474_PRG2Assignment/IceCream.cs
+++ S10262474_PRG2Assignment/S10262474_PRG2Assignment/IceCream.cs
@@ -1,13 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//==========================================================
-// Student Number : S10262474
-// Student Name : Chew Jin Xuan
-// Partner Name : Zulhimi
-//==========================================================
+
 namespace S10262474_PRG2Assignment
 {
     abstract class IceCream
@@ -28,23 +24,7 @@ namespace S10262474_PRG2Assignment
         public abstract double CalculatePrice();
         public override string ToString()
         {
-            string flavourList = string.Join(", ", Flavours.Select(f => f.Type));
-            string toppingList = Toppings.Any() ? string.Join(", ", Toppings.Select(t => t.Type)) : "None";
-
-            string details = $"Option: {Option} Scoops: {Scoops} Flavours: {flavourList} Toppings: {toppingList}";
-
-            if (this is Cone cone)
-            {
-                details += $" Dipped: {cone.Dipped}";
-            }
-            else if (this is Waffle waffle)
-            {
-                details += $" Waffle Flavour: {waffle.WaffleFlavour}";
-            }
-
-            return details;
+            return "Option: " + Option + "\tScoops: " + Scoops;
         }
     }
-
-
 }
