376bec557a44880e80f2dfd9493f3a33ebc639d7
JinXuan03 <148025005+JinXuan03@users.noreply.github.com>
2024-01-31 23:33:40 +0800

    Update Cup.cs

--- S10262474_PRG2Assignment/S10262474_PRG2Assignment/Cup.cs
+++ S10262474_PRG2Assignment/S10262474_PRG2Assignment/Cup.cs
@@ -1,9 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//==========================================================
+// Student Number : S10262474
+// Student Name : Chew Jin Xuan
+// Partner Name : Zulhimi
+//==========================================================
 namespace S10262474_PRG2Assignment
 {
     class Cup : IceCream
